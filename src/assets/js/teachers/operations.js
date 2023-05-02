4868bb438e653680915ef7025797850f922aa2a2
SantiagoGG0 <santiagogarcia337@gmail.com>
2023-05-02 18:19:52 -0500

    VEA ESTO

--- src/assets/js/teachers/operations.js
+++ src/assets/js/teachers/operations.js
@@ -6,7 +6,7 @@ import Swal from 'sweetalert2'
 import { validateForm, validateField, removeInputErrorMessage, removeErrorClassNameFields, removeErrorMessageElements } from './../utils/validations'
 // Module libraries.
 import { formElements, fieldConfigurations, getFormData, resetForm, setFormData } from './form'
-import { createTeacher, readTeachers, findTeacherById } from './repository';
+import { createTeacher, readTeachers, findTeacherById, updateTeacher, deleteTeacher } from './repository';
 
 export function listeners() {
     window.addEventListener('load', () => {
@@ -23,12 +23,20 @@ function listenFormSubmitEvent() {
         event.preventDefault();
         alertify.dismissAll();
         if (validateForm(fieldConfigurations)) {
-            createTeacher(getFormData());
+
+            const teacher = getFormData();
+            const idTeacher = formElements.fields.id.value.trim();
+
+            if (idTeacher) {
+                updateTeacher(teacher);
+            } else {
+                createTeacher(teacher);
+            }
+            
             resetForm();
             removeErrorClassNameFields('is-valid');
             alertify.success('Profesor guardado correctamente.');
             listTeachers();
-            listenFormFieldsChangeEvent();
         }
         else {
             alertify.error('Verificar los datos del formulario.')
@@ -112,11 +120,11 @@ function listTeachers() {
     }
 }
 function listenFormFieldsChangeEvent() {
-    fieldConfigurations.forEach(({ input, validations}) => {
+    fieldConfigurations.forEach(({ input, validations }) => {
         input.addEventListener('change', () => {
             removeInputErrorMessage(input);
-            
-            validations.forEach((validationConfig)=>{
+
+            validations.forEach((validationConfig) => {
                 validateField(input, validationConfig);
             })
         })
@@ -130,47 +138,65 @@ function listenFormResetEvent() {
         alertify.dismissAll();
     });
 }
+
 function listenTableClickEvent() {
     const table = document.getElementById('tblTeachers');
-    table.addEventListener('click', ({target}) => {
+    table.addEventListener('click', ({ target }) => {
         const idTeacher = target.getAttribute('data-id');
-        
-        if (target.classList.contains('btn-edit')||target.classList.contains('fa-pencil')) {
+
+        if (target.classList.contains('btn-edit') || target.classList.contains('fa-pencil')) {
             editTeacher(idTeacher);
 
-        }else if(target.classList.contains('btn-delete')||target.classList.contains('fa-trash')) {  
-            
-            Swal.fire({
-                title: '¿Estas seguro de que quieres eliminar el profesor: ?',
-                text: 'No podrás deshacer esta acción',
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#d33',
-                cancelButtonColor: '#b2b2b2',
-                confirmButtonText: 'Si, eliminar.',
-                cancelButtonText: 'Cerrar'
-
-            }).then((resultConfirm) => {
-                if(resultConfirm.isConfirmed) {
-                    console.log('Confirmar el que elimina.')
-                }else {
-                    alertify.dismissAll();
-                    alertify.message('Acción cancelada.')
-                }                   
-            })
+        } else if (target.classList.contains('btn-delete') || target.classList.contains('fa-trash')) {
+            confirmDelete(idTeacher);
         }
-    });    
+    });
 
 }
-function editTeacher(idTeacher){
-    const teacher = findTeacherById(idTeacher);
-    if (teacher){
+
+
+function editTeacher(idTeacher) {
+    const teacher = findTeacherById(parseInt(idTeacher));
+    if (teacher) {
         setFormData(teacher);
-        window.scrollTo({top: 0, behavior: 'smooth'})
-    }else{
+        window.scrollTo({ top: 0, behavior: 'smooth' })
+    } else {
         alertify.error('El profesor que selecciono no ha sido encontrado, verifique la información.')
     }
-    
+
+
+}
+
+function confirmDelete(idTeacher) {
+
+    const teacher = findTeacherById(parseInt(idTeacher));
+    if (teacher) {
+
+        Swal.fire({
+            title: `Estas seguro de que quieres eliminar el profesor:${teacher.name} `,
+            text: 'No podrás deshacer esta acción',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#d33',
+            cancelButtonColor: '#b2b2b2',
+            confirmButtonText: 'Si, eliminar.',
+            cancelButtonText: 'Cerrar'
+
+        }).then((resultConfirm) => {
+            if (resultConfirm.isConfirmed) {
+                deleteTeacher(parseInt(idTeacher));
+                listTeachers();
+                alertify.success('El registro ha sido eliminado.')
+            } else {
+                alertify.dismissAll();
+                alertify.message('Acción cancelada.');
+            }
+        });
+
+    } else {
+        alertify.error('El profesor que selecciono no ha sido encontrado, verifique la información.')
+    }
+
 
 
 }
